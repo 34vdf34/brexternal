b26f0ef2f59864fecad03ff4860a9b68e5a3361d
ra <randomartist@resilience-theatre.com>
2022-11-08 03:06:41 +0200

    * Disable journaling on firstboot.sh   * Updated nuke.sh   * Added glorytun to defconfig

--- board/raspberrypicm4io-64/rootfs_overlay_usb/usr/bin/firstboot.sh
+++ board/raspberrypicm4io-64/rootfs_overlay_usb/usr/bin/firstboot.sh
@@ -7,6 +7,9 @@ else
 	/bin/systemctl disable systemd-networkd-wait-online.service
 	echo "Disabling proftpd service"
 	/bin/systemctl disable proftpd.service
+	echo "Disabling journaling"
+	/bin/systemctl disable systemd-journald.service
+	/bin/systemctl mask systemd-journald.service
     	echo "This is first boot!"
     	cp /opt/tunnel/network-configurations/wg0.* /etc/systemd/network/
     	cp /opt/tunnel/service-files/* /etc/systemd/system/
